f0c03462355b9cc7e7527d47b9b4a4e6ee957e4f
Adolfo Gante <adolfogante@gmail.com>
2025-09-24 18:30:49 -0700

    added git command alias

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,28 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# alias
+alias bat="batcat"
+alias fzf-bat="fzf --preview 'bat --style=numbers --color=always {}'"
+
+## git 
+alias gs="git status"
+
+alias gd="git diff"
+
+alias ga="git add"
+alias gc="git commit"
+
+alias gp="git push"
+alias gu="git pull"
+
+alias gl="git log --all --graph --pretty=format:'%C(yellow)%h %C(cyan)%an %C(white)%ar %C(auto)%D %n%n%C(white)%s%n'"
+alias gb="git branch"
+alias gco="git checkout"
+
+alias gi="git init"
+alias gcl="git clone" 
+
 status --is-interactive; and oh-my-posh init fish --config /home/adoante/dotfiles/ado.omp.json | source
 
 set -gx CONDA_CHANGEPS1 false
@@ -32,10 +54,9 @@ nvm use lts > /dev/null
 set -U fish_greeting ""
 echo (set_color magenta)(/home/adoante/Repos/Untils/catmoji/catmoji.sh)(set_color normal)
 
+# setup fzf
+fzf --fish | source
+
 # zoxide
+set -x _ZO_ECHO '1'
 zoxide init fish | source
-set -x _ZO_ECHO = '1'
-
-
-# Set up fzf
-fzf_key_bindings
