6d9eace4d4f43e0bf8f076d824ba7fbc6d5a1a9b
Adolfo Gante <adolfogante@gmail.com>
2025-09-30 16:47:11 -0700

    removed conda stuff and will add back catmoji greeting

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Load environments
+bass source $HOME/.cargo/env
+
+bass source $HOME/.local/share/../bin/env
+
+# Init shell tools
+starship init fish | source
+
+fzf --fish | source
+
+set -x _ZO_ECHO 1
+zoxide init fish | source
+
+# Greeting
+set -U fish_greeting ""
+
 # alias
 alias bat="batcat"
 alias fzf-bat="fzf --preview 'bat --style=numbers --color=always {}'"
@@ -22,41 +38,4 @@ alias gb="git branch -r"
 alias gco="git checkout"
 
 alias gi="git init"
-alias gcl="git clone" 
-
-starship init fish | source
-
-set -gx CONDA_CHANGEPS1 false
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/adoante/miniconda3/bin/conda
-    eval /home/adoante/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/adoante/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/adoante/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/adoante/miniconda3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
-# Paths
-set -gx PATH $PATH /opt/nvim /usr/local/go/bin /usr/local/
-
-# Load Rust environment
-bass source $HOME/.cargo/env
-
-# Start npm and node
-nvm use lts > /dev/null
-
-# catmoji greeting"*** /ᐠ - ˕ -マ Ⳋ ***"
-set -U fish_greeting ""
-echo (set_color magenta)(/home/adoante/Repos/Untils/catmoji/catmoji.sh)(set_color normal)
-
-# setup fzf
-fzf --fish | source
-
-# zoxide
-set -x _ZO_ECHO '1'
-zoxide init fish | source
+alias gcl="git clone"
